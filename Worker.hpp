e18a44365b406775f80ecff68bae84092303ae96
Im most Phenomenaly! <136328121+Phenomenaly@users.noreply.github.com>
2026-01-08 17:27:36 +0300

    Added support for obstacles

--- Worker.hpp
+++ Worker.hpp
@@ -7,83 +7,86 @@
 #include "Archive.hpp"
 #include "EvolutionManager.hpp"
 
-
 namespace Dandelifeon {
-    inline std::unique_ptr<std::atomic<long>[]> g_thread_mana;
-    inline std::unique_ptr<std::atomic<int>[]> g_thread_blocks;
-    inline std::atomic<uint64_t> g_total_iters{ 0 };
+inline std::unique_ptr<std::atomic<long>[]> g_thread_mana;
+inline std::unique_ptr<std::atomic<int>[]> g_thread_blocks;
+inline std::atomic<uint64_t> g_total_iters{ 0 };
 
-    void workerTask(int id, Archive& archive, const Engine& engine) {
-        std::mt19937 rng(std::random_device{}() + id);
-        Genome current_gen;
+code
+Code
+download
+content_copy
+expand_less
+void workerTask(int id, Archive& archive, const Engine& engine) {
+    std::mt19937 rng(std::random_device{}() + id);
+    Genome current_gen;
 
-        current_gen.symmetric = (rng() % 2 == 0);
+    current_gen.symmetric = (rng() % 2 == 0);
 
-        // 3 structures with 8 cells each, should be moved to separate variables
-        // Also better use 2 structure with 3-5 blocks
-        for (int i = 0; i < 2; i++) {
-            Structure s;
-            s.x = 8 + rng() % 10;
-            s.y = 8 + rng() % 10;
+    // 1 - 2 structures with 3-7 cells each
+    for (int i = 0; i < 1 + rng() % 2; i++) {
+        Structure s;
+        s.x = 8 + rng() % 10;
+        s.y = 8 + rng() % 10;
 
-            // Better use 3 + rng() % 3 there
-            for (int j = 0; j < 8; j++)
-                s.addPoint(j % 3, j / 3);
+        for (int j = 0; j < 3 + rng() % 5; j++)
+            s.addPoint(j % 3, j / 3);
 
-            current_gen.organs[current_gen.organCount++] = s;
+        current_gen.organs[current_gen.organCount++] = s;
+    }
+
+    auto init_layers = current_gen.toBitboard();
+    auto best_res = engine.run(init_layers.first, init_layers.second);
+    uint64_t local_iters = 0;
+    uint64_t last_improvement = 0;
+
+    while (true) {
+        local_iters++;
+        g_total_iters.fetch_add(1, std::memory_order_relaxed);
+
+        Genome next_gen = current_gen;
+
+        int stagnation = (int)(local_iters - last_improvement);
+        int mutation_count = 1;
+
+        if (stagnation > 500'000) 
+            mutation_count = 5;
+        if (stagnation > 5'000'000)
+            mutation_count = 20;
+
+        for (int i = 0; i < mutation_count; ++i) {
+            EvolutionManager::mutate(next_gen, rng);
         }
 
-        auto best_res = engine.run(current_gen.toBitboard());
-        uint64_t local_iters = 0;
-        uint64_t last_improvement = 0;
+        auto next_layers = next_gen.toBitboard();
+        auto res = engine.run(next_layers.first, next_layers.second);
 
-        while (true) {
-            local_iters++;
-            g_total_iters.fetch_add(1, std::memory_order_relaxed);
+        if (res.fitness > best_res.fitness) {
+            current_gen = next_gen;
+            best_res = res;
+            last_improvement = local_iters;
+            current_gen.rewardLastMutation();
 
-            Genome next_gen = current_gen;
+            g_thread_mana[id].store(res.mana);
+            g_thread_blocks[id].store(res.initial_blocks);
 
-            int stagnation = (int)(local_iters - last_improvement);
-            int mutation_count = 1;
-
-            if (stagnation > 500'000) 
-                mutation_count = 3;
-            if (stagnation > 5'000'000)
-                mutation_count = 10;
-
-            for (int i = 0; i < mutation_count; ++i) {
-                EvolutionManager::mutate(next_gen, rng);
+            if (res.fitness > 10.0) {
+                engine.getPhenotype(next_layers.first, res.pheno_x, res.pheno_y);
+                archive.submit(current_gen, res);
             }
+        }
 
-            auto res = engine.run(next_gen.toBitboard());
-
-            if (res.fitness > best_res.fitness) {
-                current_gen = next_gen;
-                best_res = res;
+        if (stagnation > 200'000'000) {
+            if (archive.getElite(current_gen, rng)) {
+                auto elite_layers = current_gen.toBitboard();
+                best_res = engine.run(elite_layers.first, elite_layers.second);
                 last_improvement = local_iters;
-                current_gen.rewardLastMutation();
-
-                g_thread_mana[id].store(res.mana);
-                g_thread_blocks[id].store(res.initial_blocks);
-
-                if (res.fitness > 10.0) {
-                    engine.getPhenotype(next_gen.toBitboard(), res.pheno_x, res.pheno_y);
-                    archive.submit(current_gen, res);
-                }
             }
-
-            if (stagnation > 100'000'000) {
-                if (archive.getElite(current_gen, rng)) {
-                    best_res = engine.run(current_gen.toBitboard());
-                    last_improvement = local_iters;
-                }
-                else {
-                    current_gen = Genome();
-                }
+            else {
+                current_gen = Genome();
             }
         }
     }
-
 }
 
-
+}
